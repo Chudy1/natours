9121201b943beea5dcfee328efe24af3f24e0dda
Chudy1 <okolichukwudi34@gmail.com>
2023-06-12 10:53:31 +0100

    Initial Commit

---
+++ public/js/index.js
@@ -0,0 +1,75 @@
+/* eslint-disable */
+import { displayMap } from './mapbox';
+import { login, logout } from './login';
+import { bookTour } from './stripe';
+import { updateSettings } from './updateSettings';
+
+//DOM ELEMENT
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form--login');
+const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
+
+// DELEGATION
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
+
+if (logoutBtn) logoutBtn.addEventListener('click', logout);
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = new FormData();
+
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    console.log(form);
+
+    updateSettings(form, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    document.querySelector('.btn--save-pasword').textContent = 'Updating...';
+
+    const currentPassword = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('password-confirm').value;
+
+    await updateSettings(
+      { currentPassword, password, confirmPassword },
+      'password'
+    );
+
+    document.querySelector('.btn--save-pasword').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
+}
+
+if (bookBtn) {
+  bookBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
+  });
+}
---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+/*eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alert';
+
+const stripe = Stripe(
+  'pk_test_51LBEZMEQ4J9XHH9EYQAFt30oLtmj8u74HGW6KDfH0rtIk58QemIOR99DEO0PwlRsRtca5fgNnVa2sKxJ3RtUwERx00TLNjAneJ'
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    //   1. Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:5000/api/v1/bookings/checkout-session/${tourId}`
+    );
+
+    //   2. Create checkout form + charge credit card
+    const checkoutPageUrl = session.data.session.url;
+    window.location.assign(checkoutPageUrl);
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
